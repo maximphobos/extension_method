616ed89c1e86a51b67a8b48c3d298f00d80264d8
maximphobos <maximphobos@gmail.com>
2022-11-26 15:13:06 +0200

    Implement custom double number rounding extension method

---
+++ RoundingDoubleNumbers/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RoundingDoubleNumbers
+{
+    public static partial class Extensions
+    {
+        public static int Round(this double a)
+        {
+            return Convert.ToInt32(Math.Round(a, MidpointRounding.AwayFromZero));
+        }
+    }
+}
